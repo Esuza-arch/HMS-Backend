d3584573322287bdd1592d833ee423b86d679f3d
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2025-01-29 13:02:27 +0300

    Install packages

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_migrate import Migrate
 from models import db, Doctor, Patient, Appointment, Review 
 
@@ -7,7 +9,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+CORS(app)
 migrate = Migrate(app, db)
+
+app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Hospital Management System API"})
 
  
     
